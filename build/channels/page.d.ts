da2404f111a98181c9613e315deed1f42749898b
Max Karpawich <maxkarpawich@gmail.com>
2021-05-17 12:04:43 -0400

    chore: delete the build folder

--- build/channels/page.d.ts
+++
@@ -1,9 +0,0 @@
-import Report from '../report';
-import PollChannel from './poll';
-declare abstract class PaginationChannel extends PollChannel {
-    abstract fetchPage(): Promise<Report[]>;
-    protected lastReportDate?: Date;
-    constructor(lastReportDate?: Date);
-    fetch(): Promise<void>;
-}
-export default PaginationChannel;
--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Engine from './engine';
-import Channel from './channels/channel';
-import PaginationChannel from './channels/page';
-import PollChannel from './channels/poll';
-import Report from './report';
-export { Engine, Channel, PaginationChannel, PollChannel, Report, };
