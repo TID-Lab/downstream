13cd99ed999b53c857c34f9d3555b8ed1f4e2673
Max Karpawich <maxkarpawich@gmail.com>
2021-05-17 20:05:27 -0400

    feat: replace `downstream/builtin` with nested import

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,22 @@ import PageChannel from './channels/page';
 import PollChannel from './channels/poll';
 import Report from './report';
 
+import {
+  CrowdTangleChannel,
+  CrowdTangleFacebookChannel,
+  CrowdTangleInstagramChannel,
+  TwitterPageChannel,
+  TwitterStreamChannel,
+} from './builtin/channels';
+
+const builtin = {
+  CrowdTangleChannel,
+  CrowdTangleFacebookChannel,
+  CrowdTangleInstagramChannel,
+  TwitterPageChannel,
+  TwitterStreamChannel,
+};
+
 export {
-  Engine, Channel, PageChannel, PollChannel, Report,
+  Engine, Channel, PageChannel, PollChannel, Report, builtin,
 };
