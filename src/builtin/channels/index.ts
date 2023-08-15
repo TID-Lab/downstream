da1b6e6610c4c94b144714306adfff0ac318f89d
Max Karpawich <maxkarpawich@gmail.com>
2023-08-15 16:40:23 -0400

    feat: built-in Junkipedia channel

--- src/builtin/channels/index.ts
+++ src/builtin/channels/index.ts
@@ -5,7 +5,10 @@ import CrowdTangleInstagramChannel from './crowdtangle/instagram';
 import TwitterPageChannel from './twitter/page';
 import TwitterStreamChannel from './twitter/stream';
 
+import JunkipediaChannel from './junkipedia/junkipedia';
+
 export {
   CrowdTangleChannel, CrowdTangleFacebookChannel, CrowdTangleInstagramChannel,
   TwitterPageChannel, TwitterStreamChannel,
+  JunkipediaChannel,
 };
